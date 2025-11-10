1c1793f9f19fd985307cb28c36a1d6268413fdc7
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-11-11 06:35:12 +0900

    fix(securityConfig): 리액트 주소 허용 추가 Ref: #7

--- ZzicGo_be/src/main/java/com/ZzicGo/config/SecurityConfig.java
+++ ZzicGo_be/src/main/java/com/ZzicGo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ZzicGo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +20,9 @@ public class SecurityConfig {
             "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/swagger-ui.html"
     };
 
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
@@ -33,8 +37,9 @@ public class SecurityConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
+
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(allowedOrigin));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);  // 쿠키/인증정보 포함 요청
