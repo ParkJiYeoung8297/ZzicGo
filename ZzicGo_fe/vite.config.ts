579380105cde1b6972de657c283b8d14eec2d426
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-11-13 00:26:09 +0900

    feat: PWA 설정 추가 - 설치 시 로고 띄우기, 설치 안내 배너 띄우기  (React) Closed : #14

--- ZzicGo_fe/vite.config.ts
+++ ZzicGo_fe/vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";  // 추가
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({       // 추가
+          registerType: "autoUpdate",
+          manifest: {
+            name: "ZzicGo",
+            short_name: "ZzicGo",
+            start_url: ".",
+            display: "standalone",
+            background_color: "#000000",
+            theme_color: "#000000",
+            icons: [
+              {
+                src: "/icon-192.png",
+                sizes: "192x192",
+                type: "image/png",
+              },
+              {
+                src: "/icon-512.png",
+                sizes: "512x512",
+                type: "image/png",
+              },
+            ],
+          },
+        }),
+      ],
+    });
