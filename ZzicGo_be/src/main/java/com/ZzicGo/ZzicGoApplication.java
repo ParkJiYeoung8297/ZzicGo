de8bb96d25e8680a111d5bfb1f1433cc0d9902a4
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-11-21 05:12:32 +0900

    fix(appllication): 스프링 한국시간으로 수정 (Spring)

--- ZzicGo_be/src/main/java/com/ZzicGo/ZzicGoApplication.java
+++ ZzicGo_be/src/main/java/com/ZzicGo/ZzicGoApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class ZzicGoApplication {
-
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(ZzicGoApplication.class, args);
 	}
 
