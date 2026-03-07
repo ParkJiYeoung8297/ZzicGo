c98bd334742b8bad7b8dfb115cb682590352c98e
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-03-08 05:27:20 +0900

    feat: 서버 로그 config 추가 (Spring)

---
+++ ZzicGo_be/src/main/java/com/ZzicGo/config/LoggingConfig.java
@@ -0,0 +1,18 @@
+package com.ZzicGo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LoggingConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludeClientInfo(true);
+        filter.setIncludePayload(false);
+        return filter;
+    }
+}
