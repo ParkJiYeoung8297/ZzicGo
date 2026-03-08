f2b53926a110e4320ad90344e80b8d77cfaf81cf
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-03-08 21:25:56 +0900

    fix(#75): 403에러 반환으로 인해 토큰 재발급 안되는 오류 수정

--- ZzicGo_be/src/main/java/com/ZzicGo/config/jwt/JwtAuthenticationFilter.java
+++ ZzicGo_be/src/main/java/com/ZzicGo/config/jwt/JwtAuthenticationFilter.java
@@ -36,7 +36,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String token = authHeader.substring(7);
 
         if (!jwtProvider.validateToken(token)) {
-            filterChain.doFilter(request, response);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
 
